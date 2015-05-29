c3fa26d6755da3b59a3cb8ddf494f95e088cd448
aandrieiev <andreev00@gmail.com>
2015-05-29 15:43:20 +0200

    Add queue names to config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,9 @@
 use Mix.Config
 
-config :mailer,
-  from: "openaperture@lexmark.com"
+config :mailer, from: "openaperture@lexmark.com"
+
+config :openaperture_notifications,
+  hipchat: "notifications_hipchat",
+  email: "notifications_email"
 
 import_config "#{Mix.env}.exs"
