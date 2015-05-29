579e08a1d1b82e2eb465dcbad93dab8419d538a0
aandrieiev <andreev00@gmail.com>
2015-05-29 19:12:22 +0200

    Align values

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,6 @@ config :mailer, from: "openaperture@lexmark.com"
 
 config :openaperture_notifications,
   hipchat: "notifications_hipchat",
-  email: "notifications_email"
+  email:   "notifications_email"
 
 import_config "#{Mix.env}.exs"
